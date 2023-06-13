a5b71807f097b0afb160641369bcd38817289e14
NerdyDave2017 <nerdydave2017@gmail.com>
2023-06-13 18:17:39 +0100

    feat: remove log folfer from staging

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -54,12 +54,12 @@ const transports = [
   new winston.transports.Console(),
   // Allow to print all the error level messages inside the error.log file
   new winston.transports.File({
-    filename: "src/logs/error.log",
+    filename: "logs/error.log",
     level: "error",
   }),
   // Allow to print all the error message inside the all.log file
   // (also the error log that are also printed inside the error.log(
-  new winston.transports.File({ filename: "src/logs/all.log" }),
+  new winston.transports.File({ filename: "logs/all.log" }),
 ];
 
 // Create the logger instance that has to be exported
