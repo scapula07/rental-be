81ffe098f5c32e495d3bd4060cd488f3b7a17411
NerdyDave2017 <nerdydave2017@gmail.com>
2023-06-14 03:16:30 +0100

    feat: install swagger dependencies

---
+++ src/utils/swagger.ts
@@ -0,0 +1,5 @@
+import { Express, Request, Response } from "express";
+import swaggerJsDocs from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { version } from "../../package.json";
+import logger from "./logger";
