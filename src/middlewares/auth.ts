334805366cbb8339323150fb6724a1ed95115207
NerdyDave2017 <nerdydave2017@gmail.com>
2023-06-14 01:54:44 +0100

    feat: create auth middleware funcrion

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import logger from "../utils/logger";
+import { verifyJwt } from "../utils/jwt";
+
+export default function auth(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    logger.warn("No authorization header");
+    return res.status(401).json({ message: "No authorization header" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    logger.warn("No token provided");
+    return res.status(401).json({ message: "No token provided" });
+  }
+  const payload = verifyJwt(token);
+  if (!payload) {
+    logger.warn("Invalid token");
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  next();
+}
