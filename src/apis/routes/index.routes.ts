f07d77a6d238f9348821ba3fd495fc817b93a279
NerdyDave2017 <nerdydave2017@gmail.com>
2023-08-10 15:16:53 +0100

    refactor: modularize user service

--- src/apis/routes/index.routes.ts
+++ src/apis/routes/index.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
-import usersRoutes from "./users/users.routes";
+import usersRoutes from "../../modules/users/routes/index.routes";
 
 router.use("/users", usersRoutes);
 
