fd0a3858c9e4fb8fe9937bc4bbf204169302d95e
NerdyDave2017 <nerdydave2017@gmail.com>
2023-09-20 12:59:47 +0100

    feat: create booking schema

---
+++ src/modules/booking/models/bookings.model.ts
@@ -0,0 +1,70 @@
+import {
+  Types,
+  Schema,
+  Document,
+  model,
+  Model,
+  SchemaDefinitionProperty,
+  ObjectId,
+} from "mongoose";
+import bcrypt from "bcrypt";
+import { string } from "zod";
+
+export interface IBooking extends Document {
+  user: ObjectId;
+  car: ObjectId;
+  startDate: string;
+  endDate: string;
+  totalPrice: number;
+  paymentId: ObjectId;
+  bookingStatus: string;
+}
+
+const CarSchema = new Schema<IBooking>(
+  {
+    user: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    car: {
+      type: Types.ObjectId,
+      ref: "Car",
+      required: true,
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: String,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    paymentId: {
+      type: Types.ObjectId,
+      ref: "Payment",
+      required: true,
+    },
+    bookingStatus: {
+      type: String,
+      enum: [
+        "pending",
+        "approved",
+        "declined",
+        "cancelled",
+        "completed",
+        "active",
+      ],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Cars: Model<IBooking> = model<IBooking>("Car", CarSchema);
+
+export default Cars;
